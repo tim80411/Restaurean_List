55c6a0edab5cb85ec41b6729542f9e642b570061
tim80411 <tim80411@gmail.com>
2021-04-26 22:44:49 +0800

    Upload static file: bootstrap, popper, jquery

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
   res.render('index')
