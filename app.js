f3782093c5a98f129f10ee8990b6cc6403cd321c
tim80411 <tim80411@gmail.com>
2021-04-26 23:52:14 +0800

    Design index's layout

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 const app = express()
 const port = 3000
 
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 
