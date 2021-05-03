38fdeac04ca4da78f6226d9f5870c997a77d4233
tim80411 <tim80411@gmail.com>
2021-05-03 21:21:41 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -39,17 +39,6 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
 })
 
-
-// detail route
-app.get('/restaurants/:restaurant_id/detail', (req, res) => {
-  const id = req.params.restaurant_id
-
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('show', { restaurant }))
-    .catch(error => console.error(error))
-})
-
 // route: Add restaurant function
 app.get('/restaurants/new', (req, res) => {
   res.render('new')
@@ -67,13 +56,57 @@ app.post('/restaurants', (req, res) => {
   const rating = Number(req.body.rating)
   const description = req.body.description
 
-  Restaurant.create({
+  return Restaurant.create({
     id, name, name_en, category, image, location, phone, google_map, rating, description
   })
     .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
+//route: detail
+app.get('/restaurants/:restaurant_id/detail', (req, res) => {
+  const id = req.params.restaurant_id
+
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.error(error))
+})
+
+// route: update restaurant function
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => {
+      for (let key in restaurant) {
+        restaurant[key] = restaurant[key].toString().split(' ').join('')
+      }
+      res.render('edit', { id, restaurant })
+    })
+    .catch(error => console.error(error))
+})
+
+app.post('/restaurants/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.id = Number(req.body.id)
+      restaurant.name = req.body.name
+      restaurant.name_en = req.body.name_en
+      restaurant.category = req.body.category
+      restaurant.image = req.body.image
+      restaurant.location = req.body.lccation
+      restaurant.phone = req.body.phone
+      restaurant.google_map = req.body.google_map
+      restaurant.rating = Number(req.body.rating)
+      restaurant.description = req.body.description
+
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}/detail`))
+    .catch(error => console.error(error))
+})
 
 // search route
 app.get('/search', (req, res) => {
