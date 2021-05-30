c072115948b3c2f6a5bb152d24a5791094cd9936
Yiting Liao <tim80411@gmail.com>
2021-05-30 13:24:05 +0800

    faet: add authenticator middleware before login

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const express = require('express')
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 const router = express.Router()
 
-router.use('/restaurants', restaurants)
+router.use('/restaurants', authenticator, restaurants)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
